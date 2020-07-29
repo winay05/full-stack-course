5bb34f141506b6eb3358c6088173fbc9462710b5
winay05 <vinay.manipal05@gmail.com>
2020-07-29 06:43:40 +0530

    1. put the jQuery code in a separate script in a js folder. Include this script in all the html pages 2. use module onchange to watch for any change in any scss file, and automatically recompile it using a script. 3. use parallelshell module to run this watch as well as browsersync using lite server, using a script, say watch:all 4. run the watch:all script on start to watch for all changes in.

---
+++ week1-Bootstrap/js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({ interval: 2000});
+    
+    $('#carouselButton').click(function(){
+        if ($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else{
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+
+        
+    });
+
+    $('#loginModalButton').click(function(){
+        $('#loginModal').modal('toggle')
+    });
+
+    $('#ReservationModalButton').click(function(){
+        $('#ReservationModal').modal('toggle')
+    });
+    
+
+});
